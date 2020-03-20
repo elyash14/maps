0774485a17609bb29a68ab3b3ddb0eec8326829c
elyash14 <elyas_mosayebi@yahoo.com>
2020-03-20 20:45:49 +0330

    feat: show simple map on the screan

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
-const article = document.getElementById('app');
-article.innerHTML = 'Hello World!';
+require('@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css')
+var mapboxgl = require('mapbox-gl');
+
+
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoic21zYWVpZDY0IiwiYSI6ImNrNzdkbGc3dzAwaDQzbG52aXJ3OWE4cTgifQ.ROB6bd_fVWlA2pbyNeNLEQ";
+
+var map = new mapboxgl.Map({
+  container: "map", // container id
+  style: "mapbox://styles/mapbox/streets-v11", //hosted style id
+  center: [49.602045, 37.27071], // starting position
+  zoom: 14 // starting zoom
+});
+
