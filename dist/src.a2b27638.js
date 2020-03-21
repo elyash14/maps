a490cc1d76986e4bf1ca586af56c0d7d43f80d0e
elyash14 <elyas_mosayebi@yahoo.com>
2020-03-21 16:09:59 +0430

    feat: add some style

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -25820,7 +25820,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38543" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44393" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
